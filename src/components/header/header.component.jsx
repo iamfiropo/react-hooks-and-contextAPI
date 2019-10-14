522b23389850a34676f350d523e335efedb855b9
Johnpeace <princeolatjohnjoe@yahoo.com>
2019-10-14 15:30:11 +0100

    initial commit

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
-import { selectCartHidden } from '../../redux/cart/cart.selectors';
-import { selectCurrentUser } from '../../redux/user/user.selectors';
+
+import CurrentUserContext from '../../contexts/current-user/current-user.context';
+import { CartContext } from '../../providers/cart/cart.provider';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser, hidden }) => (
+const Header = () => {
+  const currentUser = useContext(CurrentUserContext);
+  const { hidden } = useContext(CartContext);
+
+  return (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -34,15 +37,10 @@ const Header = ({ currentUser, hidden }) => (
           SIGN IN
         </Link>
       )}
-      <CartIcon />
+        <CartIcon />
     </div>
     {hidden ? null : <CartDropdown />}
   </div>
-);
+)};
 
-const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser,
-  hidden: selectCartHidden
-});
-
-export default connect(mapStateToProps)(Header);
+export default Header;
--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,13 +1,19 @@
-import React from 'react';
-import { connect } from 'react-redux';
+import React, { useContext } from 'react';
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
-import { selectCollection } from '../../redux/shop/shop.selectors';
+import CollectionsContext from '../../contexts/collections/collections.context';
 
 import './collection.styles.scss';
 
-const CollectionPage = ({ collection }) => {
+/**
+ * 
+ * @param {*} param0 
+ * A more simpler way of leveraging context API with useContext hook
+ */
+const CollectionPage = ({ match }) => {
+  const collections = useContext(CollectionsContext);
+  const collection = collections[match.params.collectionId]
   const { title, items } = collection;
   return (
     <div className='collection-page'>
@@ -21,8 +27,34 @@ const CollectionPage = ({ collection }) => {
   );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
+/**
+ * Another way to leverage context API by wrapping the component inside powered component
+ * and access it's Consumer function
+ * @param {*} param0 
+ * 
+   const CollectionPage = ({ match }) => {
+     return (
+       <CollectionsContext.Consumer>
+         {
+           collections => {
+             const collection = collections[match.params.collectionId]
+             const { title, items } = collection;
+             return (
+               <div className='collection-page'>
+                 <h2 className='title'>{title}</h2>
+                 <div className='items'>
+                   {items.map(item => (
+                     <CollectionItem key={item.id} item={item} />
+                   ))}
+                 </div>
+               </div>
+             )
+           }
+         }
+       </CollectionsContext.Consumer>
+     );
+   };
 
-export default connect(mapStateToProps)(CollectionPage);
+ */
+
+export default CollectionPage;
